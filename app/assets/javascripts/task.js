6e1dbc4e14ca97f8608f8a33c3adf3962ded71cd
koto338 <monmo4909@gmail.com>
2021-07-18 11:59:25 +0900

    Ajaxでタスクを削除JS

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -11,3 +11,20 @@ document.addEventListener('turbolinks:load', function() {
     });
   });
 });
+
+// 削除したタスクを非表示にする
+document.addEventListener('turbolinks:load', function() {
+  // document.querySelectorAll('.delete')は削除リンクの要素群を返す。
+  // forEachを使って、各要素にaddEventListener()を使ってajax:successイベントに対応する処理を定義
+  document.querySelectorAll('.delete').forEach(function(a) {
+    a.addEventListener('ajax:success', function() {
+      // parentNodeは要素の親要素を返す
+      // 削除リンクを表すオブジェクトがaという変数に入り、parentNodeで削除リンクの親要素、td要素を返す
+      var td = a.parentNode;
+      // tdの親要素tr要素を取得
+      var tr = td.parentNode;
+    　// tr要素のスタイルを変更して非表示
+      tr.style.display = 'none';
+    });
+  });
+});
